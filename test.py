e6f6f1b4553639f1929d2bfbfdfdce4be94865d6
lynnlau <271307356@qq.com>
2018-10-30 17:41:16 +0800

    新文件：   command 	新文件：   test.py

---
+++ test.py
@@ -0,0 +1,87 @@
+#!/user/python/env
+#
+import sys
+﻿import serial
+
+def Translate(s):
+    tmp = []
+    for i in s:
+        if i == ' ':
+            pass
+        elif i == '\n':
+            break
+        else:
+            try:
+                tmp += [int(i,16)]
+            except BaseException as err:
+                return None , err
+    if len(tmp)%2:
+        return None,'The larg of command is incorrect'
+    data = []
+    i = 0
+    while tmp[i:i+2]:
+        data += [(tmp[i]<<4)+tmp[i+1]]
+        i += 2
+    return data,None
+
+def Test(c):
+    global ser
+    ser.write(c)
+    while True:
+        if self.ser.inWaiting () == 0:
+            if OUTSTREAM != '':
+                OutFile = open(OutPut,'a')
+                OutFile.write(OUTSTREAM)
+                OutFile.close()
+                OUTSTREAM = ''
+            else:
+                pass
+        else:
+            time.sleep(0.2)
+            data = self.ser.read(ser.inWaiting())
+            data = list(data)
+            print('')
+            print ('RX:',end=' ')
+
+if __name__ == '__main__':
+    global ser
+
+    Pare = sys.argv[1]
+    
+    ser = serial.Serial()
+    ser. parity='E'
+    ser.port= '/dev/ttyUSB2'
+    try:
+        ser.open()
+    except BaseException as e:
+        print (e)
+        
+    with open('./'+Pare) as file:
+        NumofLine = 1
+        CommandArray = []
+        while True:
+            tmp = file.readline()
+            if tmp == '':
+                break
+            elif tmp =='\n':
+                pass
+            else:
+                Command ,err = Translate(tmp)
+                if err:
+                    print('line',NumofLine,': invalid command in identifier')
+                    print('information:',err)
+                    print('Please press any key to exit')
+                    tmp = input()
+                    exit()
+                elif Command == None:
+                    pass
+                else:
+                    CommandArray += [Command]
+            NumofLine += 1
+    print(CommandArray)
+    for i in CommandArray:
+        Test(i)
+
+
+
+
